c58daae57247092c211696b24964b167e2c6316a
Abul Hasan Lakhani <abulhasanlakhani@gmail.com>
2018-12-02 16:18:20 +1300

    Get the trimmed and filtered path from the URL

--- server/index.js
+++ server/index.js
@@ -5,9 +5,19 @@
 */
 
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World');
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+
+    // Filter non-required characters
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    res.end(`Trimmed Path is: ${trimmedPath}`);
 });
 
 server.listen(3000, () => {
